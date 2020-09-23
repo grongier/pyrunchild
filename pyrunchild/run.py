9de2ea06578b3ca2e9bba4cd565293c6ce0fe86b
Rongier, Guillaume (Data61, Kensington WA) <Guillaume.Rongier@data61.csiro.au>
2020-09-23 17:34:06 +0800

    Continued cleaning pyrunchild.

--- pyrunchild/run.py
+++ pyrunchild/run.py
@@ -1,26 +1,6 @@
 """CHILD runner"""
 
-# The MIT License (MIT)
-# Copyright (c) 2020 CSIRO
-#
-# Author: Guillaume Rongier
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# LICENCE GOES HERE
 
 
 import os
@@ -80,44 +60,6 @@ class Child(InputWriter):
                              seed=seed)
 
         self.child_executable = os.path.expanduser(child_executable)
-        
-    def talkative_run(self,
-                      realization=0,
-                      input_name=None,
-                      silent_mode=False,
-                      write_log=False):
-        
-        if input_name is None:
-            if self.base_names[realization] is None:
-                self.write_input_file(realization)
-            input_name = self.base_names[realization]
-
-        options = ''
-        if silent_mode == True:
-            options = '--silent-mode'
-
-        with ExitStack() as stack:
-
-            log_file = None
-            if write_log == True:
-                log_file = stack.enter_context(open(os.path.join(self.base_directory,
-                                                                 input_name + '.log'),
-                                                    'w'))
-            process = stack.enter_context(subprocess.Popen([self.child_executable,
-                                                            options,
-                                                            input_name + '.in'],
-                                                           cwd=self.base_directory,
-                                                           stdout = subprocess.PIPE, 
-                                                           stderr = subprocess.DEVNULL))
-
-            for line in iter(process.stdout.readline, b""):
-                print(line.strip().decode('ascii'))
-                if log_file is not None:
-                    log_file.write(line.strip().decode('ascii') + '\n')
-
-        if process.returncode != 0:
-            raise subprocess.CalledProcessError(process.returncode,
-                                                process.args)
 
     def run(self,
             realization=0,
@@ -132,7 +74,47 @@ class Child(InputWriter):
             save_previous_input_file=True,
             extensions_to_remove=None,
             return_parameter_values=False):
+        """
+        Runs CHILD.
+        
+        Parameters
+        ----------
+        realization : int (default 0)
+            Realization to run.
+        input_name : str, optional (default None)
+            Name of a specific input file to run (must be in the base directory).
+        silent_mode : bool (default False)
+            If true, runs CHILD in silent mode.
+        print_log : bool (default False)
+            If true, prints the log at the end of the simulation.
+        write_log : bool (default False)
+            If true, writes the log into a file.
+        timeout : float, optional (default None)
+            Maximum run time for the simulation.
+        update_seed : bool (default True)
+            If true and the simulation fails or times out, try again with a
+            different seed in CHILD.
+        resolve_parameters : bool (default True)
+            If true and the simulation fails or times out, try again with a
+            different parameter values. Only works if some of the parameters
+            are randomly drawn.
+        max_attempts : int (default 1)
+            Maximum number of other attempts if the simulation fails or times
+            out.
+        save_previous_input_file : bool (default True)
+            If true and another simulation is required, save the input files of
+            the failed simulation.
+        extensions_to_remove : list of str, optional (default None)
+            Files to suppress at the end of the simulation.
+        return_parameter_values : bool (default False)
+            Return the parameter values of the simulation.
 
+        Returns
+        -------
+        dict
+            The parameter values, only if return_parameter_values is true.
+            
+        """
         random_state = np.random.RandomState(self.seed + realization)
         if input_name is None:
             if self.base_names[realization] is None:
@@ -229,7 +211,39 @@ class Child(InputWriter):
                   save_previous_input_file=True,
                   extensions_to_remove=None,
                   max_attempts=1):
-
+        """
+        Runs multiple CHILD simulations in parallel.
+        
+        Parameters
+        ----------
+        n_jobs : int (default -1)
+            Number of jobs to use for the computation. -1 means using all
+            available processors.
+        chunksize : int (default 1)
+            Size of each chunk sent to the jobs.
+        print_log : bool (default False)
+            If true, prints the log at the end of the simulation.
+        write_log : bool (default False)
+            If true, writes the log into a file.
+        timeout : float, optional (default None)
+            Maximum run time for the simulation.
+        update_seed : bool (default True)
+            If true and the simulation fails or times out, try again with a
+            different seed in CHILD.
+        resolve_parameters : bool (default True)
+            If true and the simulation fails or times out, try again with a
+            different parameter values. Only works if some of the parameters
+            are randomly drawn.
+        save_previous_input_file : bool (default True)
+            If true and another simulation is required, save the input files of
+            the failed simulation.
+        extensions_to_remove : list of str, optional (default None)
+            Files to suppress at the end of the simulation.
+        max_attempts : int (default 1)
+            Maximum number of other attempts if the simulation fails or times
+            out.
+            
+        """
         if n_jobs == -1:
             try:
                 n_jobs = int(os.environ["SLURM_NTASKS"])
@@ -254,29 +268,137 @@ class Child(InputWriter):
             self.base_names, self.parameter_values = zip(*results)
             self.base_names = list(self.base_names)
             self.parameter_values = list(self.parameter_values)
+
+    def talkative_run(self,
+                      realization=0,
+                      input_name=None,
+                      silent_mode=False,
+                      write_log=False):
+        """
+        Runs CHILD while displaying the log information as it comes.
+        
+        Parameters
+        ----------
+        realization : int (default 0)
+            Realization to run.
+        input_name : str, optional (default None)
+            Name of a specific input file to run (must be in the base directory).
+        silent_mode : bool (default False)
+            If true, runs CHILD in silent mode.
+        write_log : bool (default False)
+            If true, writes the log into a file.
+            
+        """
+        if input_name is None:
+            if self.base_names[realization] is None:
+                self.write_input_file(realization)
+            input_name = self.base_names[realization]
+
+        options = ''
+        if silent_mode == True:
+            options = '--silent-mode'
+
+        with ExitStack() as stack:
+
+            log_file = None
+            if write_log == True:
+                log_file = stack.enter_context(open(os.path.join(self.base_directory,
+                                                                 input_name + '.log'),
+                                                    'w'))
+            process = stack.enter_context(subprocess.Popen([self.child_executable,
+                                                            options,
+                                                            input_name + '.in'],
+                                                           cwd=self.base_directory,
+                                                           stdout = subprocess.PIPE, 
+                                                           stderr = subprocess.DEVNULL))
+
+            for line in iter(process.stdout.readline, b""):
+                print(line.strip().decode('ascii'))
+                if log_file is not None:
+                    log_file.write(line.strip().decode('ascii') + '\n')
+
+        if process.returncode != 0:
+            raise subprocess.CalledProcessError(process.returncode,
+                                                process.args)
     
     def generate_nodes(self,
                        OUTFILENAME='nodes',
                        OPTINITMESHDENS=0,
-                       X_GRID_SIZE=10000,
-                       Y_GRID_SIZE=10000,
+                       X_GRID_SIZE=10000.,
+                       Y_GRID_SIZE=10000.,
                        OPT_PT_PLACE=1,
-                       GRID_SPACING=200,
+                       GRID_SPACING=200.,
                        NUM_PTS='n/a',
                        TYP_BOUND=1,
                        NUMBER_OUTLETS=0,
                        OUTLET_X_COORD='n/a',
                        OUTLET_Y_COORD='n/a',
-                       MEAN_ELEV=0,
-                       RAND_ELEV=1,
+                       MEAN_ELEV=0.,
+                       RAND_ELEV=1.,
                        SLOPED_SURF=0,
-                       UPPER_BOUND_Z=0,
-                       OPTINLET=0,
-                       INLET_X='n/a',
-                       INLET_Y='n/a',
-                       SEED=100,
+                       UPPER_BOUND_Z=0.,
+                       SEED=42,
                        delete_files=True):
+        """
+        Runs CHILD over a null period of time just to get the nodes. It lets us
+        modify those nodes before using them in another simulation.
         
+        Parameters
+        ----------
+        OUTFILENAME : str (default 'nodes')
+            Base name for output files.
+        OPTINITMESHDENS : int (default 0)
+            Option for densifying the initial mesh by inserting a new node at
+            the circumcenter of each triangle. The value of this parameter is
+            the number of successive densification passes (for example, if 2,
+            then the mesh is densified twice).
+        X_GRID_SIZE : float (default 10000.)
+            Total length of model domain in x direction (m).
+        Y_GRID_SIZE : float (default 10000.)
+            Total length of model domain in y direction (m).
+        OPT_PT_PLACE : int (default 1)
+            Method of placing points when generating a new mesh:
+            0 = uniform hexagonal mesh;
+            1 = regular staggered (hexagonal) mesh with small random offsets in
+                (x, y) positions;
+            2 = random placement.
+        GRID_SPACING : float (default 200.)
+            Mean distance between grid nodes (m).
+        NUM_PTS : int (default 'n/a')
+            Number of points in grid interior, if random point positions are used.
+        TYP_BOUND : int (default 1)
+            Configuration of boundaries with a rectangular mesh:
+            0 = open boundary in one corner;
+            1 = open boundary along x = 0;
+            2 = open boundaries along x = 0 and x = xmax;
+            3 = open boundaries along all four sides;
+            4 = single open boundary node at specified coordinates.
+        NUMBER_OUTLETS : int (default 0)
+            Number of outlets.
+        OUTLET_X_COORD : float (default 'n/a')
+            x coordinate of single-node outlet (open boundary) (m).
+        OUTLET_Y_COORD : float (default 'n/a')
+            y coordinate of single-node outlet (open boundary) (m).
+        MEAN_ELEV : float (default 0.)
+            Mean elevation of initial surface (m).
+        RAND_ELEV : float (default 1.)
+            Maximum amplitude of random variations in initial node elevations (m).
+        SLOPED_SURF : int (default 0)
+            Option for initial sloping surface (downward toward y = 0).
+        UPPER_BOUND_Z : float (default 0.)
+            If sloping initial surface is applied, this sets the slope by setting
+            the altitude of the model edge at y = ymax (m).
+        SEED : int (default 42)
+            Seed for random number generation. Must be an integer.
+        delete_files : bool (default True)
+            If true, deletes the files generated by CHILD to create the nodes.
+
+        Returns
+        -------
+        nodes : ndarray, shape (n, 5)
+            The nodes.
+            
+        """
         node_writer = InputWriter(self.base_directory, OUTFILENAME)
         node_writer.set_run_control(RUNTIME=0,
                                     OPINTRVL=1,
@@ -295,10 +417,7 @@ class Child(InputWriter):
                                    MEAN_ELEV=MEAN_ELEV,
                                    RAND_ELEV=RAND_ELEV,
                                    SLOPED_SURF=SLOPED_SURF,
-                                   UPPER_BOUND_Z=UPPER_BOUND_Z,
-                                   OPTINLET=OPTINLET,
-                                   INLET_X=INLET_X,
-                                   INLET_Y=INLET_Y)
+                                   UPPER_BOUND_Z=UPPER_BOUND_Z)
         node_writer.set_tectonics(OPTNOUPLIFT=1)
         node_writer.set_fluvial_transport(OPTNOFLUVIAL=1)
         node_writer.set_hillslope_transport(OPTNODIFFUSION=1)
@@ -318,6 +437,8 @@ class Child(InputWriter):
         return nodes[0]
     
     def clean_directory(self):
-        
+        """
+        Cleans the base directory of any file generated by CHILD.
+        """
         subprocess.call('rm ' + self.base_directory + 'run.time', shell=True)
         subprocess.call('rm ' + self.base_name + '*', shell=True)
